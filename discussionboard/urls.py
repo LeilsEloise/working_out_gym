5ef4035420e64366933c2bac6edd4aa1c58702eb
LeilsEloise <leilahhodgson@gmail.com>
2026-03-02 19:45:19 +0000

    further updates made to discussionboard app will complete tomorrow morning

--- discussionboard/urls.py
+++ discussionboard/urls.py
@@ -1,10 +1,13 @@
 #ChatGPT Acknowledgement
 from django.urls import path
-from . import views
+from .views import PostList, PostDetail, PostCreate, PostUpdate, PostDelete
 
 app_name = "discussionboard"
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="post_list"),
-    path("vote/<slug:slug>/<str:value>/", views.post_vote, name="post_vote"),
+ path("", PostList.as_view(), name="post_list"),
+    path("new/", PostCreate.as_view(), name="post_create"),
+    path("<slug:slug>/", PostDetail.as_view(), name="post_detail"),
+    path("<slug:slug>/edit/", PostUpdate.as_view(), name="post_edit"),
+    path("<slug:slug>/delete/", PostDelete.as_view(), name="post_delete"),
 ]
--- discussionboard/views.py
+++ discussionboard/views.py
@@ -1,19 +1,69 @@
-# ChatGPT Code
+#ChatGPT code
 
-from django.contrib.auth.decorators import login_required
 from django.views import generic
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.utils.text import slugify
 
 from .models import Post
-
 
 class PostList(generic.ListView):
     model = Post
     template_name = "discussionboard/post_list.html"
     context_object_name = "posts"
-    ordering = ["-id"]  # safe even if you don't have created_on
+    paginate_by = 10
+    ordering = ["-created_on"]  # if you have created_on, otherwise use ["-id"]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            context["my_posts"] = Post.objects.filter(
+                author=self.request.user
+            ).order_by("-created_on")
+        else:
+            context["my_posts"] = Post.objects.none()
+
+        return context    
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "discussionboard/post_detail.html"
+    context_object_name = "post"
+
+class PostCreate(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    fields = ["title", "content"]  # add fields you actually have
+    template_name = "discussionboard/post_form.html"
+    success_url = reverse_lazy("discussionboard:post_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        if not form.instance.slug:
+            form.instance.slug = slugify(form.instance.title)
+        return super().form_valid(form)
+
+class PostOwnerOrSuperuserMixin(UserPassesTestMixin):
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user.is_superuser or post.author == self.request.user
+
+class PostUpdate(LoginRequiredMixin, PostOwnerOrSuperuserMixin, generic.UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    template_name = "discussionboard/post_form.html"
+
+    def get_success_url(self):
+        return reverse_lazy("discussionboard:post_detail", kwargs={"slug": self.object.slug})
+
+class PostDelete(LoginRequiredMixin, PostOwnerOrSuperuserMixin, generic.DeleteView):
+    model = Post
+    template_name = "discussionboard/post_confirm_delete.html"
+    success_url = reverse_lazy("discussionboard:post_list")
 
 
 @login_required
