419a3ab99af76af94d4282c05de5a2be24c983a6
LeilsEloise <leilahhodgson@gmail.com>
2026-03-05 10:39:04 +0000

    Updated database to new PostgreSQL URL after previous was compromised, have started to build my Merchandise app, I have imported products from a CSV off Kaggle

--- working_out_gym/settings.py
+++ working_out_gym/settings.py
@@ -125,12 +125,17 @@ WSGI_APPLICATION = 'working_out_gym.wsgi.application'
 #    }
 #}
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {"default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
 
 
 CSRF_TRUSTED_ORIGINS = [
