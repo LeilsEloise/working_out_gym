6a777b30c6ebfdbd341472aa5bccec15ce2e65d7
LeilsEloise <leilahhodgson@gmail.com>
2026-03-05 22:25:34 +0000

    Products now displaying in correct grid layout for different screen sizes

--- merchandise/views.py
+++ merchandise/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.db.models import Min
 from .models import Product
 
 # Create your views here.
 def all_products(request):
     """ A view to show list of all Products"""
-
-    products = Product.objects.all()
+# ChatGPT Update
+    products = (
+        Product.objects.filter(is_active=True)
+        .select_related("category")
+        .annotate(from_price=Min("variants__price"))
+    )
 
     context = {
         'products': products
