284d773198f1ea51a306134e98876b4c7ddebfba
LeilsEloise <leilahhodgson@gmail.com>
2026-03-19 10:51:01 +0000

    Added add to bag and keep shopping buttons on product_details.html and created view and url for add_to_bag function

--- shoppingbag/urls.py
+++ shoppingbag/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.view_shoppingbag, name="view_shoppingbag"),
+    path('add/<product_id>/', views.add_to_bag, name='add_to_bag'),
 ]
--- shoppingbag/views.py
+++ shoppingbag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ShoppingBagItem
 
 # Create your views here.
@@ -14,3 +14,19 @@ def view_shoppingbag(request):
     }
 
     return render(request, 'shoppingbag/shoppingbag.html', context)
+
+def add_to_bag(request, product_id):
+    """ A view to allow users to add products to shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] += quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
